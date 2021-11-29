396fa0b231aede9f188deffe6ed1cf1a19eed272
Aleksandr <a.y.ivlev@gmail.com>
2021-11-29 15:57:28 +0300

    Added version app v1.0.1

--- staticsrv/cmd/staticsrv/main.go
+++ staticsrv/cmd/staticsrv/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
+	"context"
+	"fmt"
 	"github.com/kelseyhightower/envconfig"
+	"log"
+	"os"
+	"os/signal"
+	"staticsrv/internal/api/server"
+	"staticsrv/internal/app/version"
+	"syscall"
 )
 
 // Config задает параметры конфигурации приложения
 type Config struct {
 	Port        string `envconfig:"PORT" default:"8080"`
 	StaticsPath string `envconfig:"STATICS_PATH" default:"./static"`
+	//StaticsPath string `envconfig:"STATICS_PATH" default:"../test_static"`
 }
 
 func main() {
@@ -20,15 +26,49 @@ func main() {
 		log.Fatalf("Can't process config: %v", err)
 	}
 
-	r := http.NewServeMux()
-	r.Handle("/__heartbeat__", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-	fs := http.FileServer(http.Dir(config.StaticsPath))
-	r.Handle("/", fs)
+	//r := http.NewServeMux()
+	//r.Handle("/__heartbeat__", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	//fs := http.FileServer(http.Dir(config.StaticsPath))
+	//r.Handle("/", fs)
 
-	log.Printf("start server on port: %s", config.Port)
-	err = http.ListenAndServe(":"+config.Port, r)
-	if err != nil {
-		log.Fatalf("Error while serving: %v", err)
+	//go func() {
+	//	log.Printf("start server on port: %s", config.Port)
+	//	err = http.ListenAndServe(":"+config.Port, r)
+	//	if err != nil {
+	//		log.Fatalf("Error while serving: %v", err)
+	//	}
+	//}()
+
+	info := server.VersionInfo{
+		Version: version.Version,
+		Commit:  version.Commit,
+		Build:   version.Build,
 	}
 
+	srv := server.New(info, config.Port, config.StaticsPath)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		err := srv.Serve(ctx)
+		if err != nil {
+			log.Printf("start server on port: %s", config.Port)
+			log.Println(fmt.Errorf("serve: %w", err))
+			return
+		}
+	}()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case killSignal := <-interrupt:
+		switch killSignal {
+		case os.Interrupt:
+			log.Print("Got SIGINT...")
+		case syscall.SIGTERM:
+			log.Print("Got SIGTERM...")
+		}
+	}
+
+	cancel()
 }
